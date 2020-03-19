e5510ed222c8a286018246359653cde99b6fb94b
Javier Guerra <javier.guerra@konghq.com>
2020-03-19 08:26:29 -0500

    new "just trace" macro

--- src/errtrace.h
+++ src/errtrace.h
@@ -8,6 +8,7 @@
 #define none ((void)0)
 
 #define T()	fprintf(stderr, "[%s:%d] %s: %s\n", __FILE__, __LINE__, __func__, strerror(errno))
+#define Tp(fmt, ...)	fprintf(stderr, "[%s:%d] %s: " fmt "\n", __FILE__, __LINE__, __func__, __VA_ARGS__)
 #define Tv(v) ({T(); (v);})
 #define Tf(l) ({T(); goto l;})
 #define Tcont() ({T(); continue;})
