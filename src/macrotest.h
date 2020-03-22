d16a7cfd30e9ac33e3ec010a1dad2c319d886c21
Javier Guerra <javier.guerra@konghq.com>
2020-03-21 21:47:37 -0500

    inner test scoping

--- src/macrotest.h
+++ src/macrotest.h
@@ -4,12 +4,15 @@
 
 #define begin_test(name)	int main () {
 #define end_test	fprintf(stderr, "OK\n"); return 0; }
-
+#define do_test(msg)	\
+	for(int __k = (fprintf(stderr, msg) && 0); \
+		__k == 0; __k = (fprintf(stderr, "   OK\n"), 1))
 
 
 #else		// no TEST
 
 #define begin_test(name)	static int __no_call__() {
 #define end_test	return 0; }
+#define do_test(msg)	if(0)
 
 #endif		// TEST
